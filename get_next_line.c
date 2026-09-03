32c253e2fbb6dd12c82f27e1277a51b6c393c0a6
Samuel Pedro <samupedr@student.42luanda.com>
2026-09-03 15:28:02 +0100

    add get_next_line skeleton

---
+++ get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samupedr <samupedr@student.42luanda.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/03 14:49:17 by samupedr          #+#    #+#             */
+/*   Updated: 2026/09/03 15:25:40 by samupedr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*buf;
+
+	if (fd < 0)
+		return (NULL);
+	buf = (char *)malloc(BUFFER_SIZE);
+	if (!buf)
+		return (NULL);
+	return (gnl_read_line(&buf, fd));
+}
+
+static char	*gnl_read_line(char **buf, int fd)
+{
+	t_string	s;
+
+	s = gnl_create_string(*buf);
+	while (read(fd, *buf, BUFFER_SIZE) > 0)
+		gnl_string_append(s, gnl_next_chunk(buf));
+}
