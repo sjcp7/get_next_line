f6752c0084936d3ad2f8904bff531519c58aea5b
Samuel Pedro <samupedr@student.42luanda.com>
2026-09-03 15:27:33 +0100

    introduce a dynamic string type

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: samupedr <samupedr@student.42luanda.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/03 14:37:09 by samupedr          #+#    #+#             */
-/*   Updated: 2026/09/03 14:42:08 by samupedr         ###   ########.fr       */
+/*   Updated: 2026/09/03 15:26:53 by samupedr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4096
 # endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_string
+{
+	char	*s;
+	size_t	capacity;
+	size_t	len;
+}	t_string;
 
 char	*get_next_line(int fd);
 
