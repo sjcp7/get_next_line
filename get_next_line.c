36c6b545801c3d4471af98bf51c54cca20d43a92
Samuel Pedro <samupedr@student.42luanda.com>
2026-09-07 11:30:20 +0100

    zero-initialize gnl buffer

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: samupedr <samupedr@student.42luanda.com>   +#+  +:+       +#+        */
+/*   By: samupedr <samupedr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/03 14:49:17 by samupedr          #+#    #+#             */
-/*   Updated: 2026/09/03 15:28:45 by samupedr         ###   ########.fr       */
+/*   Updated: 2026/09/07 11:29:48 by samupedr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 char	*get_next_line(int fd)
 {
 	static char	*buf;
+	size_t		i;
 
 	if (fd < 0)
 		return (NULL);
-	buf = (char *)malloc(BUFFER_SIZE);
+	buf = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
+	i = 0;
+	while (i <= BUFFER_SIZE)
+		buf[i++] = '\0';
 	return (gnl_read_line(&buf, fd));
 }
 
@@ -30,5 +34,5 @@ static char	*gnl_read_line(char **buf, int fd)
 
 	s = gnl_create_string(*buf, BUFFER_SIZE);
 	while (read(fd, *buf, BUFFER_SIZE) > 0)
-		gnl_string_append(s, gnl_next_chunk(buf));
+		gnl_string_append(&s, gnl_next_chunk(buf));
 }
